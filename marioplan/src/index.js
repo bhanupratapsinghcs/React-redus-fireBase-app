0aa5e854adc200a113abe6fbdc30a7308abad343
Bhanu pratap singh <pbhanu314@gmail.com>
2021-05-08 00:31:59 +0530

    thunk middleware to dispatch

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './store/reducers/rootReducer'
-import thunk from 'react-thunk'
+import thunk from 'redux-thunk'
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
 
