88c5b7e97d79f5f976753e8974d94dc6b4678f41
Bhanu pratap singh <pbhanu314@gmail.com>
2021-05-07 23:52:11 +0530

    store added with redux reducer

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'react-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ marioplan/src/store/reducers/projectReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+    projects: [
+        { id: '1', title: 'help me find peach', content: 'blah blah blah' },
+        { id: '2', title: 'collect all the stars', content: 'blah blah blah' },
+        { id: '4', title: 'egg hunt with bhanu', content: 'blah blah blah' }
+    ]
+}
+
+const projectReducer = (state = initState, action) => {
+    return state
+}
+export default projectReducer
