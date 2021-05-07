8c3ddff258d7c73fa53062c9848b7ab78b0f75e9
Bhanu pratap singh <pbhanu314@gmail.com>
2021-05-07 23:50:56 +0530

    create project component added

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -5,6 +5,8 @@ import DashBoard from './Components/Dashboard/DashBoard'
 import ProjectDetails from './Components/projects/ProjectDetails'
 import SignIn from './Components/Auth/SignIn'
 import SignUp from './Components/Auth/SignUp'
+import CreateProject from './Components/projects/CreateProject'
+
 class App extends Component {
   render() {
     return (
@@ -16,6 +18,7 @@ class App extends Component {
             <Route path='/project/:id' component={ProjectDetails} />
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
+            <Route path='/createproject' component={CreateProject} />
           </Switch>
         </div>
       </BrowserRouter>
