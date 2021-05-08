bcad6085504714f7d9344f185270a7fcd78eaa62
Bhanu pratap singh <pbhanu314@gmail.com>
2021-05-09 01:36:52 +0530

    fireStore added to store date in db

--- marioplan/src/store/action/projectAction.js
+++ marioplan/src/store/action/projectAction.js
@@ -1,6 +1,18 @@
 export const createproject = (project) => {
-    return (dispatch, getState, { getfirebase, getfirestore }) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // for async call too database
-        dispatch({ type: 'CREATE_PROJECT', project })
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: "bhanu",
+            authorLastName: "singh",
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+        })
+
     }
 }
